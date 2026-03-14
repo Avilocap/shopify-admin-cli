c98cc27995e5638c6503a2c09dd22f10a7040668
David De la Torre <david@daviddelatorre.me>
2026-03-14 22:05:24 +0100

    Bump version to 0.1.6 in CLI program

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ const program = new Command();
 program
   .name("shopfleet")
   .description("Private CLI for managing Shopify stores")
-  .version("0.1.5")
+  .version("0.1.6")
   .option("--store <alias>", "Configured store alias");
 
 registerConfigCommands(program);
